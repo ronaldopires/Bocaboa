aa36555c37fe74e4894f624685ec6a388644c83f
Ronaldo Carvalho <ronaldo.carvalho@hotmail.com>
2022-03-26 00:48:45 -0300

    Aula 05 finalizando

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector('#main-header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const links = document.querySelectorAll('nav ul li a')
+const header = document.querySelector('#main-header')
+const navHeight = header.offsetHeight
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
@@ -15,8 +17,6 @@ for (const link of links) {
 }
 /* Scroll Header */
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#main-header')
-  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -36,6 +36,7 @@ function backToTop() {
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuCurrent()
 })
 
 /* Swiper JS */
@@ -45,25 +46,61 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* Scroll Reveal JS */
-const scrollReveal = ScrollReveal({
-  origin: 'bottom',
-  distance: '50px',
-  duration: 700,
+ScrollReveal({
+  duration: 900,
+  delay: 400,
+  distance: '60px',
   reset: true
 })
 
-scrollReveal.reveal(
-  `
-  #home .image, #home .text,
-  #about .image, #about .text,
-  #services header, #services .card,
-  #testimonials header, #testimonials .testimonials,
-  #contact .text, #cotact .links,
-  footer .brand, footer .social
-  `,
-  { interval: 100 }
+ScrollReveal().reveal('#about .image, #testimonials header, .form', {
+  delay: 400,
+  origin: 'top'
+})
+ScrollReveal().reveal('#home .text, #services .card, #contact .links', {
+  origin: 'left'
+})
+ScrollReveal().reveal(
+  '#home .image, #services header, #contact .text, footer .social',
+  {
+    delay: 150,
+    origin: 'right'
+  }
 )
+ScrollReveal().reveal(
+  '#about .text, #testimonials .testimonials, footer .brand',
+  {
+    delay: 200,
+    origin: 'bottom'
+  }
+)
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuCurrent() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 9) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
